b2a97ffb402fc185280e52e92f944e71a4bc9a83
T. Lanette <TLPollard4516@gmail.com>
2020-02-28 18:04:43 -0500

    Re-doing Carousel
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,25 +1,2 @@
-/*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
-    */
 
-/* Carousel */
 
-$('#carouselIndicators').on('slide.bs.carousel', function (e) {
-    let $e = $(e.relatedTarget);
-    let idx = $e.index();
-    let itemsPerSlide = 4;
-    let totalItems = $('.carousel-item').length;
-
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        let it = itemsPerSlide - (totalItems - idx);
-        for (let i=0; i<it; i++) {
-            //append slides to the end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');  
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-});
