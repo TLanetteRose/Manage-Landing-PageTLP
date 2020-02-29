f55b6e07b51f00134ed4c05b94098b9fe0cf8d64
T. Lanette <TLPollard4516@gmail.com>
2020-02-29 13:31:35 -0500

    Updating carousel
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 
 /* Carousel */
-$('.carousel.carousel-multi-item .carousel-item').each(function() {
-    let next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
 
-    for (var i=0;<4;i++) {
-        next=next.next();
-        if (!next.length) {
-            next=$(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-    }
-});
 
