e1e0358677d40b9eeff274c48f4c4258b9acfcc9
T. Lanette <TLPollard4516@gmail.com>
2020-02-28 19:57:52 -0500

    Added JS for Carousel
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 
+/* Carousel */
+$('.carousel.carousel-multi-item .carousel-item').each(function() {
+    let next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
 
-
+    for (var i=0;<4;i++) {
+        next=next.next();
+        if (!next.length) {
+            next=$(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
 
