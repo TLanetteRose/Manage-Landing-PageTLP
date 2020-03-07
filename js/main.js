b566aba98aaaa728f3528d4a9f03a71e00b1b7b2
T. Lanette <TLPollard4516@gmail.com>
2020-03-07 10:01:11 -0500

    Redo of glide carousel
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
 
 /* Carousel */
-new Glide ('.testimonialCarousel', {
-   type: 'carousel',
-   startAt: 0,
-   perView: 4,
-   focusAt: 'center',
-   gap: 50,
-   
-   breakpoints: {
-       770: {
-           perView: 1
-       }
-   }
-})
+
