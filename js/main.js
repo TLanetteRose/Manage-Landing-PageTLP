fc3fdf61502bd739486b497fb3ce7841ba95204a
T. Lanette <TLPollard4516@gmail.com>
2020-03-07 17:32:13 -0500

    Erased Testimonial Slider
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,20 +1,2 @@
 
 /* Carousel */
-const testCarousel = new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    gap: 20,
-    perView: 4,
-    breakpoints: {
-        1024: {
-            perView: 3
-        },
-        800: {
-            perView: 2
-        },
-        600: {
-            perView: 1
-        }
-    }
-})
-testCarousel.mount();
