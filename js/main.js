a642d9eaf1f1ecb47acd54e5d22908e14661b8c9
T. Lanette <TLPollard4516@gmail.com>
2020-03-06 20:19:53 -0500

    Added Glide carousel
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 
 /* Carousel */
-
+new Glide ('.testimonialCarousel', {
+   type: 'carousel',
+   startAt: 0,
+   perView: 4,
+   focusAt: 'center',
+   gap: 50,
+   
+   breakpoints: {
+       770: {
+           perView: 1
+       }
+   }
+})
