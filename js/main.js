a331ad72bb1301aa5d23c8da6aa55fba29be719b
T. Lanette <TLPollard4516@gmail.com>
2020-03-07 16:28:49 -0500

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 
 /* Carousel */
+const carousel = new Glide('.testimonials', {
+    type: 'carousel',
+    startAt: 0,
+    gap: 20,
+    perView: 4,
+    breakpoints: {
+        1024: {
+            perView: 3
+        },
+        800: {
+            perView: 2
+        },
+        600: {
+            perView: 1
+        }
+    }
+});
+carousel.mount();
 
