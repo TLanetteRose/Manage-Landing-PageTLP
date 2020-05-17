93cd818c5d803cce5a8393a9f1aa6bb9b07a60c8
T. Lanette <TLPollard4516@gmail.com>
2020-05-17 15:25:13 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ const testCarousel = new Glide('.testimonial', {
 })
 testCarousel.mount();
 
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const error = document.querySelector('.error');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInput();
+});
+
+function checkInput() {
+    // trim to remove the whitespaces
+    const emailValue = email.value.trim();
+
+    if (emailValue === '' || !isEmail(emailValue)) {
+        error.style.visibility = 'visible';
+        email.classList.remove('error-input');
+    } else {
+        error.style.visibility = "hidden";
+        email.classList.remove('error-input');
+    }
+}
+
